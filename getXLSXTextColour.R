bbdae9efc3d42f72f09f35529e85230a369294b3
seannyD <sean.g.roberts@gmail.com>
2017-07-14 18:19:47 +0100

    slightly more commenting

--- getXLSXTextColour.R
+++ getXLSXTextColour.R
@@ -3,10 +3,6 @@
 library(xlsx)
 # Hint:  after making a CellStyle object called cellStyle, run names(cellStyle) to get a list of functions that can be run to get different properties of the font/background colour
 
-filename = "~/Desktop/TextColours.xlsx"
-outfilename = "~/Desktop/TextColours_withCategories.csv"
-columnToConvert = 2
-
 getCellFontColour = function(cell){
   # Font colours can either be indexed or full hex values
   cellStyle = getCellStyle(cell)
@@ -19,20 +15,35 @@ getCellFontColour = function(cell){
   }
 }
 
+# the file to convert
+filename = "~/Desktop/TextColours.xlsx"
+# the file to write the results to
+outfilename = "~/Desktop/TextColours_withCategories.csv"
+#  the column you want to convert in the file.
+columnToConvert = 2
 
 
+# Load the workbook (we need to do it this way to get at the formatting options)
 wb <- xlsx::loadWorkbook(filename)
+# Get the sheets in the workbook
 sheets <- getSheets(wb)
+# Get the first sheet
 sheet <- sheets[[1]]
+# get rows in the sheet (the data for all rows)
 rows <- getRows(sheet)
+# get the data in the column to be converted
 cells <- getCells(rows, colIndex =columnToConvert)
 
+# Get the text colours in the column
 colours = sapply(cells,getCellFontColour)
 # cut out the first row (header)
 colours = colours[2:length(colours)]
+# Convert the colours to an integer 
 colour.categories = as.integer(as.factor(colours))
 
-# Read in the data as a data frame, 
+# Read in the data (again, but as a data frame)
 wb2 = xlsx::read.xlsx(filename, sheetIndex=1, header=TRUE)
+# Add the extra column of colours converted to an integer
 wb2 = cbind(wb2, colour.categories)
+# Write out the data to a new file
 write.csv(wb2, file=outfilename)
